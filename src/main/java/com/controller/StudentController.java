2af67f3212a5dc7f50c074b7562c310e05b6ff31
Tomasz <sobeczki@gmail.com>
2022-03-17 20:05:11 +0000

    created basic student class

---
+++ src/main/java/com/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import com.StudentRepository;
+import com.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://192.168.178.50:4200")
+@RequestMapping("api")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository employeeRepository;
+
+    //GetAllEmployees
+    @GetMapping("employee")
+    public List<Student> getAllEmployees(){
+        return  employeeRepository.findAll();
+    }
+
+
+}
---
+++ src/main/java/com/model/Student.java
@@ -0,0 +1,75 @@
+package com.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity()
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "county")
+    private String county;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    @Column(name = "birth")
+    private LocalDate birth;
+
+
+    public Student() {  }
+
+    public Student(String firstName, String lastName) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public String getCounty() { return county; }
+    public void setCounty(String county) { this.county = county; }
+
+    public String getTown() {  return town; }
+    public void setTown(String town) { this.town = town; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public LocalDate getBirth() { return birth; }
+    public void setBirth(LocalDate birth) { this.birth = birth; }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmailId() { return emailId; }
+    public void setEmailId(String emailId) { this.emailId = emailId; }
+}
