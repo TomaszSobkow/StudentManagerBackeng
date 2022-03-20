58cd227e6a69d2c752baaf7297e78949b6ea579d
Tomasz <sobeczki@gmail.com>
2022-03-20 23:01:35 +0000

    added Post API

--- src/main/java/com/controller/StudentController.java
+++ src/main/java/com/controller/StudentController.java
@@ -3,8 +3,9 @@ package com.controller;
 import com.StudentRepository;
 import com.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,17 @@ import java.util.List;
 public class StudentController {
 
     @Autowired
-    private StudentRepository employeeRepository;
+    private StudentRepository studentRepository;
 
     //GetAllEmployees
     @GetMapping("/students")
     public List<Student> getAllEmployees(){
-        return  employeeRepository.findAll();
+        return  studentRepository.findAll();
+    }
+
+    //Add new Student
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student newStudent){
+        return studentRepository.save(newStudent);
     }
 }
