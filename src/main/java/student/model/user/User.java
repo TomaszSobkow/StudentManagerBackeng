1e1b2b29380b46c113b7a9af64c50c164b8eb5a8
TomaszSobkow <sobeczki@gmail.com>
2023-03-29 19:29:53 +0100

    added lombok to project and setup user class to lombok

--- src/main/java/student/model/user/User.java
+++ src/main/java/student/model/user/User.java
@@ -1,10 +1,19 @@
 package student.model.user;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
