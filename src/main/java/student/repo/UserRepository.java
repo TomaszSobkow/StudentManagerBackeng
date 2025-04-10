2c7aef0ce8a6777c98b9ad56fcc5e3692751785c
TomaszSobkow <sobeczki@gmail.com>
2025-04-10 14:04:19 +0100

    Removed unuadssdasdassed imports

--- src/main/java/student/repo/UserRepository.java
+++ src/main/java/student/repo/UserRepository.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import student.model.User;
 
 import java.util.List;
-
+@CrossOrigin
 @Repository
 public class UserRepository {
 
